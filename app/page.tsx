eb8d6216acdb4bd7c5769459d3be2ffeb5b802e3
heesang-kim <heesang.kim@navercorp.com>
2023-08-25 20:11:37 +0900

    Add history state testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,25 @@
+'use client';
+
+import Link from "next/link";
+import {useEffect} from "react";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  const loggingFooHistoryState = () => {
+    console.log(history.state.foo)
+  }
+  const handleClick = () => {
+    history.replaceState({...history.state, foo: 'foo'}, '', location.href)
+  }
+
+  useEffect(() => {
+    loggingFooHistoryState()
+  }, []);
+
+  return <div>
+    <Link onClick={handleClick} href='https://nextjs.org/'>Next.js Page</Link>
+    <br/>
+    <button onClick={handleClick}>set history state</button>
+    <button onClick={loggingFooHistoryState} >logging history state</button>
+  </div>
 }
